4b0f5c1b735436899bb8b7c6d40028463bf44842
Tsi2216 <getanehshimelis40@gmail.com>
2025-08-16 12:52:51 -0700

    added task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,7 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Show the create table statement for the Books table
-SHOW CREATE TABLE Books;
+-- Select the full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
